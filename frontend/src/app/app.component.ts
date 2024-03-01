52b49241a0d6fd83408ef90e25211c37d3a4b05b
Ferdous Rahsepar <rahsepar19@gmail.com>
2024-03-01 03:10:04 +0100

    Aktualisierung der Angular-Abhängigkeiten und Import von TodoComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { TodoComponent } from './features/components/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule,TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
