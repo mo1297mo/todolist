dfb43b97406009df1566fd0ea56dfadc2a3ddb4c
Ferdous Rahsepar <rahsepar19@gmail.com>
2024-03-23 15:25:55 +0100

    minor fix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://hammerhead-app-4gzpl.ondigitalocean.app', // Development API URL
+    apiUrl: 'https://hammerhead-app-4gzpl.ondigitalocean.app/api/todos', // Development API URL
   };
   
