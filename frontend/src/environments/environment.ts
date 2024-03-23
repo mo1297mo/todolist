e372d1dfeba0e1f4430fc34ffb4f7403a565ffbe
Ferdous Rahsepar <rahsepar19@gmail.com>
2024-03-23 15:15:12 +0100

    Aktualisiere die API-URL in der Entwicklungsumgebung auf https://hammerhead-app-4gzpl.ondigitalocean.app

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/todos', // Development API URL
+    apiUrl: 'https://hammerhead-app-4gzpl.ondigitalocean.app', // Development API URL
   };
   
